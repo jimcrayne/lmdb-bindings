b555dde15b0b698ad4994410cc1dcb23e428780c
James Crayne <jim.crayne@gmail.com>
2017-06-14 00:58:02 +0000

    Beginning of testsuite

---
+++ test/test-lmdb-bindings.hs
@@ -0,0 +1,16 @@
+
+import Data.Binary
+import qualified Data.ByteString.Arbitrary
+import qualified Data.ByteString.Char8 as B
+import Data.List
+import Data.Word
+import System.Directory
+import System.FilePath
+import System.IO.Temp
+import System.Process
+import Test.Hspec
+
+import PackUtf8
+import Database.LMDB
+
+main = putStrLn "todo"
